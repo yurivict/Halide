e919055fd2fc190e13d202c9306acc4accb6aac1
Alex Reinking <alex.reinking@gmail.com>
2020-05-04 12:23:29 -0700

    Add C backend support.

--- cmake/HalideGeneratorHelpers.cmake
+++ cmake/HalideGeneratorHelpers.cmake
@@ -30,19 +30,19 @@ define_property(TARGET PROPERTY HLRT_TARGETS
                 FULL_DOCS "On a Halide runtime target, lists the targets the runtime backs")
 
 function(add_halide_library TARGET)
-    set(options GRADIENT_DESCENT)
+    set(options GRADIENT_DESCENT C_BACKEND)
     set(oneValueArgs FROM GENERATOR FUNCTION_NAME USE_RUNTIME PYTHON_EXTENSION SCHEDULER REGISTRATION)
     set(multiValueArgs PARAMS TARGETS FEATURES PLUGINS)
     cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    if (NOT ARG_FROM)
+        message(FATAL_ERROR "Missing FROM argument specifying a Halide generator target")
+    endif ()
 
     if (ARG_GRADIENT_DESCENT)
         set(GRADIENT_DESCENT 1)
     else ()
         set(GRADIENT_DESCENT 0)
-    endif ()
-
-    if (NOT ARG_FROM)
-        message(FATAL_ERROR "Missing FROM argument specifying a Halide generator target")
     endif ()
 
     if (NOT ARG_GENERATOR)
@@ -62,6 +62,7 @@ function(add_halide_library TARGET)
         set(generatorCommand ${CMAKE_COMMAND} -E env "PATH=$<SHELL_PATH:$<TARGET_FILE_DIR:Halide::Halide>>" "$<TARGET_FILE:${ARG_FROM}>")
     endif ()
 
+    unset(EXTRA_RT_LIBS)
     if (opengl IN_LIST ARG_FEATURES)
         if (NOT TARGET X11::X11)
             message(AUTHOR_WARNING "OpenGL with Halide requires target X11::X11. Attempting to find_package(X11) and create target X11::X11.")
@@ -93,52 +94,66 @@ function(add_halide_library TARGET)
     endforeach ()
     string(REPLACE ";" "," TARGETS "${TARGETS}")
 
-    if (NOT ARG_USE_RUNTIME)
-        add_library("${TARGET}.runtime" STATIC IMPORTED)
-        target_link_libraries("${TARGET}.runtime"
-                              INTERFACE
-                              Threads::Threads
-                              ${CMAKE_DL_LIBS}
-                              ${EXTRA_RT_LIBS})
-
-        set_target_properties("${TARGET}.runtime"
-                              PROPERTIES
-                              IMPORTED_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.runtime${CMAKE_STATIC_LIBRARY_SUFFIX}")
-
-        add_custom_command(OUTPUT "${TARGET}.runtime${CMAKE_STATIC_LIBRARY_SUFFIX}"
-                           COMMAND ${generatorCommand} -r "${TARGET}.runtime" -o . target=$<JOIN:$<TARGET_PROPERTY:${TARGET}.runtime,HLRT_TARGETS>,$<COMMA>>
-                           DEPENDS "${ARG_FROM}")
-
-        add_custom_target("${TARGET}.runtime.update"
-                          DEPENDS "${TARGET}.runtime${CMAKE_STATIC_LIBRARY_SUFFIX}")
-
-        add_dependencies("${TARGET}.runtime" "${TARGET}.runtime.update")
-        set(ARG_USE_RUNTIME "${TARGET}.runtime")
+    if (ARG_C_BACKEND AND ARG_USE_RUNTIME)
+        message(WARNING "Warning: the C backend does not use a runtime.")
     endif ()
 
-    if (NOT TARGET ${ARG_USE_RUNTIME})
-        message(FATAL_ERROR "Invalid runtime target ${ARG_USE_RUNTIME}")
+    if (NOT ARG_C_BACKEND)
+        if (NOT ARG_USE_RUNTIME)
+            add_library("${TARGET}.runtime" STATIC IMPORTED)
+            target_link_libraries("${TARGET}.runtime"
+                                  INTERFACE
+                                  Threads::Threads
+                                  ${CMAKE_DL_LIBS}
+                                  ${EXTRA_RT_LIBS})
+
+            set_target_properties("${TARGET}.runtime"
+                                  PROPERTIES
+                                  IMPORTED_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.runtime${CMAKE_STATIC_LIBRARY_SUFFIX}")
+
+            add_custom_command(OUTPUT "${TARGET}.runtime${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                               COMMAND ${generatorCommand} -r "${TARGET}.runtime" -o . target=$<JOIN:$<TARGET_PROPERTY:${TARGET}.runtime,HLRT_TARGETS>,$<COMMA>>
+                               DEPENDS "${ARG_FROM}")
+
+            add_custom_target("${TARGET}.runtime.update"
+                              DEPENDS "${TARGET}.runtime${CMAKE_STATIC_LIBRARY_SUFFIX}")
+
+            add_dependencies("${TARGET}.runtime" "${TARGET}.runtime.update")
+            set(ARG_USE_RUNTIME "${TARGET}.runtime")
+        endif ()
+
+        if (NOT TARGET ${ARG_USE_RUNTIME})
+            message(FATAL_ERROR "Invalid runtime target ${ARG_USE_RUNTIME}")
+        endif ()
+
+        # Add in the runtime targets but first remove features that should not be attached to a runtime
+        # TODO: The fact that profile being here fixes a linker error on Windows smells like a bug.
+        #       It complains about a symbol being duplicated between the runtime and the object.
+        set(RT_TARGETS ${TARGETS})
+        foreach (T IN ITEMS user_context no_asserts no_bounds_query no_runtime profile)
+            string(REPLACE "-${T}" "" RT_TARGETS "${RT_TARGETS}")
+        endforeach ()
+
+        set_property(TARGET "${ARG_USE_RUNTIME}" APPEND PROPERTY HLRT_TARGETS "${RT_TARGETS}")
+    else ()
+        # The C backend does not provide a runtime.
+        unset(ARG_USE_RUNTIME)
     endif ()
-
-    # Add in the runtime targets but first remove features that should not be attached to a runtime
-    # TODO: The fact that profile being here fixes a linker error on Windows smells like a bug.
-    #       It complains about a symbol being duplicated between the runtime and the object.
-    set(RT_TARGETS ${TARGETS})
-    foreach (T IN ITEMS user_context no_asserts no_bounds_query no_runtime profile)
-        string(REPLACE "-${T}" "" RT_TARGETS "${RT_TARGETS}")
-    endforeach ()
-
-    set_property(TARGET "${ARG_USE_RUNTIME}" APPEND PROPERTY HLRT_TARGETS "${RT_TARGETS}")
 
     ##
     # Handle extra outputs
     ##
 
-    set(GENERATOR_OUTPUTS static_library c_header registration)
-    set(GENERATOR_OUTPUT_FILES
-        "${TARGET}${CMAKE_STATIC_LIBRARY_SUFFIX}"
-        "${TARGET}.h"
-        "${TARGET}.registration.cpp")
+    set(GENERATOR_OUTPUTS c_header)
+    set(GENERATOR_OUTPUT_FILES "${TARGET}.h")
+
+    if (ARG_C_BACKEND)
+        list(APPEND GENERATOR_OUTPUTS c_source)
+        list(APPEND GENERATOR_OUTPUT_FILES "${TARGET}.halide_generated.cpp")
+    else ()
+        list(APPEND GENERATOR_OUTPUTS static_library)
+        list(APPEND GENERATOR_OUTPUT_FILES "${TARGET}${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    endif ()
 
     if (ARG_PYTHON_EXTENSION)
         set(${ARG_PYTHON_EXTENSION} "${TARGET}.py.cpp" PARENT_SCOPE)
@@ -148,6 +163,8 @@ function(add_halide_library TARGET)
 
     if (ARG_REGISTRATION)
         set(${ARG_REGISTRATION} "${TARGET}.registration.cpp" PARENT_SCOPE)
+        list(APPEND GENERATOR_OUTPUT_FILES "${TARGET}.registration.cpp")
+        list(APPEND GENERATOR_OUTPUTS registration)
     endif ()
 
     unset(GEN_SCHEDULER)
@@ -159,7 +176,11 @@ function(add_halide_library TARGET)
     # Main library target for filter.
     ##
 
-    add_library("${TARGET}" STATIC IMPORTED)
+    if (ARG_C_BACKEND)
+        add_library("${TARGET}" STATIC "${TARGET}.halide_generated.cpp")
+    else ()
+        add_library("${TARGET}" STATIC IMPORTED)
+    endif ()
 
     # load the plugins and setup dependencies
     unset(GEN_PLUGINS)
@@ -195,7 +216,10 @@ function(add_halide_library TARGET)
     list(TRANSFORM GENERATOR_OUTPUT_FILES PREPEND "${CMAKE_CURRENT_BINARY_DIR}/")
     add_custom_target("${TARGET}.update" DEPENDS ${GENERATOR_OUTPUT_FILES})
 
-    set_target_properties("${TARGET}" PROPERTIES IMPORTED_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    if (NOT ARG_C_BACKEND)
+        set_target_properties("${TARGET}" PROPERTIES IMPORTED_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    endif ()
+
     add_dependencies("${TARGET}" "${TARGET}.update")
 
     target_include_directories("${TARGET}" INTERFACE "${CMAKE_CURRENT_BINARY_DIR}")
